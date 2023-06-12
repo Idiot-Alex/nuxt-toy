7fc199f4265d4fbf81d3042ec73c412a3a7814f7
Eleven <idiotalex@163.com>
2023-06-12 16:34:16 +0800

    add vue-flow/additional-components

--- web/app.vue
+++ web/app.vue
@@ -1,37 +1,38 @@
 <script setup>
-import { VueFlow } from '@vue-flow/core'
-import { Background } from '@vue-flow/background'
-import { MiniMap } from '@vue-flow/minimap'
-import { Controls } from '@vue-flow/controls'
+import { VueFlow, MarkerType } from '@vue-flow/core'
+import { Background, MiniMap, Controls } from '@vue-flow/additional-components'
+import '@vue-flow/core/dist/style.css'
+/* import the default theme (optional) */
+import '@vue-flow/core/dist/theme-default.css'
 
 const elements = ref([
-  // Nodes
-  // An input node, specified by using `type: 'input'`
-  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
-
-  // Default nodes, you can omit `type: 'default'`
-  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, },
-  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
-
-  // An output node, specified by using `type: 'output'`
-  { id: '4', type: 'output', label: 'Node 4', position: { x: 400, y: 200 } },
-
-  // Edges
-  // Most basic edge, only consists of an id, source-id and target-id
-  { id: 'e1-3', source: '1', target: '3' },
-
-  // An animated edge
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 0 }, class: 'light' },
+  { id: '2', type: 'output', label: 'Node 2', position: { x: 100, y: 100 }, class: 'light' },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 }, class: 'light' },
+  { id: '4', label: 'Node 4', position: { x: 150, y: 200 }, class: 'light' },
+  { id: '5', type: 'output', label: 'Node 5', position: { x: 300, y: 300 }, class: 'light' },
   { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e1-3', label: 'edge with arrowhead', source: '1', target: '3', markerEnd: MarkerType.ArrowClosed },
+  {
+    id: 'e4-5',
+    type: 'step',
+    label: 'step-edge',
+    source: '4',
+    target: '5',
+    style: { stroke: 'orange' },
+    labelBgStyle: { fill: 'orange' },
+  },
+  { id: 'e3-4', type: 'smoothstep', label: 'smoothstep-edge', source: '3', target: '4' },
 ])
 </script>
 
 <template>
   <div>
-    <UCard zoom-1>
-      <VueFlow fit-view-on-init v-model="elements" :default-viewport="{ zoom: 1.5 }" :min-zoom="0.2" :max-zoom="4">
+    <UCard>
+      <VueFlow fit-view-on-init v-model="elements">
         <Background />
-        <MiniMap pannable zoomable />
-        <Controls showZoom showFitView />
+        <MiniMap />
+        <Controls />
       </VueFlow>
     </UCard>
     <!-- <NuxtPage /> -->
@@ -39,12 +40,4 @@ const elements = ref([
 </template>
 
 <style scoped>
-/* import the required styles */
-@import "@vue-flow/core/dist/style.css";
-/* import the default theme (optional) */
-@import "@vue-flow/core/dist/theme-default.css";
-/* import default minimap styles */
-@import "@vue-flow/minimap/dist/style.css";
-/* import default controls styles */
-@import "@vue-flow/controls/dist/style.css";
 </style>
